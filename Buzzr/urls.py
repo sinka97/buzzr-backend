c92f52403d203efec56a4f73b05e2cf7f2bf7349
Thant Sin Khine Aung <sin.thant@hyphengroup.io>
2022-05-28 14:34:14 +0800

    Added a test webpage

--- Buzzr/urls.py
+++ Buzzr/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views.user import create_user, get_user_by_token, login, logout, user_state
+from .views.test import test_webpage
 
 urlpatterns = [
     # user
@@ -9,6 +10,7 @@ urlpatterns = [
     path('user/logout/', logout, name="logout"),
     path('user/state/', user_state, name="user_state"),
     
-    # 
+    # test webpage
+    path('test/webpage', test_webpage, name='test_webpage'),
     
 ]
