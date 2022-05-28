c8a8862e075659fefc6ed3ac99a6d7459e219a7c
Thant Sin Khine Aung <sin.thant@hyphengroup.io>
2022-05-28 14:55:53 +0800

    Test html

--- Buzzr/views/test.py
+++ Buzzr/views/test.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def test_webpage(request):
-    return HttpResponse("Hello World!")
+    latest_question_list = ["Is this working?", "Are we working?", "Should we rethink us?"]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'test/index.html', context)
